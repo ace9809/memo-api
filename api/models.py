855d6dc91e27914d09bc89b4376ba830bc83afcf
ace9809 <tktmdwns@naver.com>
2018-09-10 03:34:11 +0900

    Fix:title,description 기본값 설정

--- api/models.py
+++ api/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Memo(models.Model):
-    title = models.CharField(max_length=256)
-    description = models.CharField(max_length=2048)
+    title = models.CharField(max_length=256, default='title')
+    description = models.CharField(max_length=2048, default='')
 
     created = models.DateField(auto_now_add=True)
     updated = models.DateField(auto_now=True)
